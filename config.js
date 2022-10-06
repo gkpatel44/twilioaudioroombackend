a9e90813b703d6676a21df5b088ca3299907e1fc
blugee <blugee>
2022-10-06 16:27:52 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twilio: {
+    accountSid: "AC8d2aa7ec8d71c946ea457603f97a1011",
+    apiKey: "SK7f4ac86510646141ec28e79a2e999015",
+    apiSecret: "AyC5l8yyROZck7AWvcJbu0skoqh6JKtV"
+  }
+};
